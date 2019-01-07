579bfe1064afa5fd4d98bbf32c5564779b7eca98
Jagraj Singh <jagraj.khera0001@gmail.com>
2019-01-07 11:54:58 -0600

    "fixed some errors"

--- sw.js
+++ sw.js
@@ -15,6 +15,16 @@ self.addEventListener('install', function(event){
                     './img/8.jpg',
                     './img/9.jpg',
                     './img/10.jpg',
+                    './restaurant.html?id=1',
+                    './restaurant.html?id=2',
+                    './restaurant.html?id=3',
+                    './restaurant.html?id=4',
+                    './restaurant.html?id=5',
+                    './restaurant.html?id=6',
+                    './restaurant.html?id=7',
+                    './restaurant.html?id=8',
+                    './restaurant.html?id=9',
+                    './restaurant.html?id=10',
                     './js/main.js',
                     './js/dbhelper.js',
                     './js/restaurant_info.js',
@@ -26,8 +36,6 @@ self.addEventListener('install', function(event){
                     'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
                     'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js',
                     'http://localhost:5500/data/restaurants.json',
-                    ' http://localhost:5500/data/restaurants.json',
-                    'http://localhost:5500/data/restaurants.json',
                     'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?access_token=pk.eyJ1IjoiamFncmFqNTI1NCIsImEiOiJjanB5Nm1ydDgxbDE2NDJudG5sNHhmeDA1In0.ORTfhQOgg1-UY4cE8MHy8A',
                     'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1539.jpg70?access_token=pk.eyJ1IjoiamFncmFqNTI1NCIsImEiOiJjanB5Nm1ydDgxbDE2NDJudG5sNHhmeDA1In0.ORTfhQOgg1-UY4cE8MHy8A',
                     'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1540.jpg70?access_token=pk.eyJ1IjoiamFncmFqNTI1NCIsImEiOiJjanB5Nm1ydDgxbDE2NDJudG5sNHhmeDA1In0.ORTfhQOgg1-UY4cE8MHy8A',
@@ -37,7 +45,8 @@ self.addEventListener('install', function(event){
                     'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1204/1540.jpg70?access_token=pk.eyJ1IjoiamFncmFqNTI1NCIsImEiOiJjanB5Nm1ydDgxbDE2NDJudG5sNHhmeDA1In0.ORTfhQOgg1-UY4cE8MHy8A',
                     'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1207/1540.jpg70?access_token=pk.eyJ1IjoiamFncmFqNTI1NCIsImEiOiJjanB5Nm1ydDgxbDE2NDJudG5sNHhmeDA1In0.ORTfhQOgg1-UY4cE8MHy8A',
                     'https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png',
-                    ' https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png'
+                    ' https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png',
+                    ' http://127.0.0.1:5500/manifest.json'
 
                 ]
             );
